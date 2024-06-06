7a4260b67d9330cf806e3b5be94e95cf20da6cbd
Christopher Youn <s3660619@csitprdap03.int.its.rmit.edu.au>
2024-06-06 23:07:09 +1000

    Added a scripts folder and functions.php for global use

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -1,40 +1,5 @@
 <?php
-session_start();
-if (isset($_COOKIE['userConsent']) && $_COOKIE['userConsent'] === 'true') {
-    echo '<script src="additionalScript.js" defer></script>';
-}
-
-function isUserLoggedIn() {
-    return isset($_SESSION['user_id']);
-}
-
-function getProfileOptions() {
-    if (isUserLoggedIn()) {
-        return '
-                <a href="../login/profile.php">View Profile</a>
-                <a href="../login/logout.php">Sign Out</a>
-                ';
-    } else {
-        return '
-                <a href="../login/login.php">Sign In</a>
-                <a href="../login/register.php">Sign Up</a>
-                ';
-    }
-}
-
-function getProfileFooter() {
-    if (isUserLoggedIn()) {
-        return '
-                <a href="../login/profile.php">Profile</a>
-                <a href="../cart/cart.ph">Cart</a>
-                ';
-    } else {
-        return '
-                <a href="../login/login.php">Sign In</a>
-                <a href="../cart/cart.ph">Cart</a>
-                ';
-    }
-}
+include('../scripts/functions.php');
 ?>
 
 <!DOCTYPE HTML>
