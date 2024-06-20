d66dca55f293294d376391cf0a6d9e05781b2a4a
Braiden Webb <s4032004@csitprdap01.int.its.rmit.edu.au>
2024-06-20 22:35:30 +1000

    remove card number and info validation

--- cart/checkout_validate.php
+++ cart/checkout_validate.php
@@ -22,68 +22,8 @@ function validateEmail($email) {
     }
 }
 
-function validateCardNumber($cardNumber) {
-    // Remove spaces and hyphens
-    $cardNumber = str_replace([' ', '-'], '', $cardNumber);
-    if (!ctype_digit($cardNumber)) {
-        return "Card number must contain only digits.";
-    }
 
-    // Use Luhn's algorithm to validate the card number
-    $sum = 0;
-    $numDigits = strlen($cardNumber);
-    $parity = $numDigits % 2;
 
-    for ($i = 0; $i < $numDigits; $i++) {
-        $digit = $cardNumber[$i];
-        if ($i % 2 == $parity) {
-            $digit *= 2;
-            if ($digit > 9) {
-                $digit -= 9;
-            }
-        }
-        $sum += $digit;
-    }
-
-    if ($sum % 10 == 0) {
-        return true;
-    } else {
-        return "Invalid card number.";
-    }
-}
-function validatecvc($cvc) {
-    if (ctype_digit($cvc) && (strlen($cvc) == 3 || strlen($cvc) == 4)) {
-        return true;
-    } else {
-        return "Invalid CVC. It must be 3 or 4 digits.";
-    }
-}
-function validateCardDate($expDate) {
-    // Check if the date is in mm/yy format
-    if (!preg_match("/^(0[1-9]|1[0-2])\/\d{2}$/", $expDate)) {
-        return "Invalid expiration date format. Use mm/yy.";
-    }
-
-    // Split the date into month and year
-    list($month, $year) = explode('/', $expDate);
-
-    // Prefix the year with 20 to make it a full year
-    $year = '20' . $year;
-
-    // Check if the month is valid
-    if ($month < 1 || $month > 12) {
-        return "Invalid expiration month.";
-    }
-
-    $currentYear = date('Y');
-    $currentMonth = date('m');
-
-    if ($year < $currentYear || ($year == $currentYear && $month < $currentMonth)) {
-        return "Card has expired.";
-    }
-
-    return true;
-}
     
 
 
@@ -98,29 +38,12 @@ if (isset($_POST['email'])) {
 } else {
     $email = 0;
 }
-if (isset($_POST['cardNo'])) {
-    $cardNumber = $_POST['cardNo'];
-} else {
-    $cardNumber = 0;
-}
-if (isset($_POST['cardDate'])) {
-    $cardDate = $_POST['cardDate'];
-} else {
-    $cardDate = 0;
-}
-if (isset($_POST['cvc'])) {
-    $cvc = $_POST['cvc'];
-} else {
-    $cvc = 0;
-}
+
 
 $fullNameValidation = validateFullName($fullName);
 $emailValidation = validateEmail($email);
-$cardNumberValidation = validateCardNumber($cardNumber);
-$cardDateValidation = validateCardDate($cardDate);
-$cvcValidation = validatecvc($cvc);
 
-if ($fullNameValidation === true && $emailValidation === true && $cardNumberValidation === true) {
+if ($fullNameValidation === true && $emailValidation === true) {
     $_SESSION['orderValidated'] = true;
     header("Location: order_finished.php");
     exit;
